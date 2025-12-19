c255a276946d1bf0554ab91fa58a8b94800901d2
rnagal741 <rnagal741@cable.comcast.com>
2025-12-19 10:43:27 -0500

    new tests added

--- src/test/java/com/rdk/validation/FrameworkSmokeTest.java
+++ src/test/java/com/rdk/validation/FrameworkSmokeTest.java
@@ -3,6 +3,7 @@ package com.rdk.validation;
 import com.rdk.validation.base.BaseTest;
 import org.testng.annotations.Test;
 import org.testng.Assert;
+import com.rdk.validation.simulator.TVsimulator;
 
 public class FrameworkSmokeTest extends BaseTest {
 
@@ -10,6 +11,23 @@ public class FrameworkSmokeTest extends BaseTest {
     public void frameworkShouldRun() {
         System.out.println("TestNG framework is running successfully");
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void smokeSimulatorTest() {
+        TVsimulator tv = new TVsimulator();
+
+        tv.powerOn();
+        Assert.assertTrue(tv.isOn());
+
+        tv.connectWifi();
+        Assert.assertTrue(tv.isWifiConnected());
+
+        tv.launchApp("Netflix");
+        Assert.assertEquals(tv.getCurrentApp(), "Netflix");
+
+        tv.powerOff();
+        Assert.assertFalse(tv.isOn());
     }
 }
  
---
+++ src/test/java/com/rdk/validation/simulator/TVsimulator.java
@@ -0,0 +1,58 @@
+package com.rdk.validation.simulator;
+
+public class TVsimulator {
+
+    private boolean isOn = false;
+    private boolean isWifiConnected = false;
+    private String currentApp = "";
+
+    // Power methods
+    public void powerOn() {
+        isOn = true;
+        System.out.println("TV powered ON");
+    }
+
+    public void powerOff() {
+        isOn = false;
+        currentApp = "";
+        System.out.println("TV powered OFF");
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    // WiFi methods
+    public void connectWifi() {
+        if (!isOn) {
+            System.out.println("Cannot connect WiFi: TV is OFF");
+            return;
+        }
+        isWifiConnected = true;
+        System.out.println("WiFi connected");
+    }
+
+    public void disconnectWifi() {
+        isWifiConnected = false;
+        System.out.println("WiFi disconnected");
+    }
+
+    public boolean isWifiConnected() {
+        return isWifiConnected;
+    }
+
+    // App methods
+    public void launchApp(String appName) {
+        if (!isOn) {
+            System.out.println("Cannot launch app: TV is OFF");
+            return;
+        }
+        currentApp = appName;
+        System.out.println("App launched: " + appName);
+    }
+
+    public String getCurrentApp() {
+        return currentApp;
+    }
+}
+
